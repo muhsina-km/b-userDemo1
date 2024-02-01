f54d9b62011d830d245b4c9f7b31cfe53fc451b6
muhsina-km <127017116+muhsina-km@users.noreply.github.com>
2024-02-01 15:20:22 +0530

    Add files via upload

--- model/register.js
+++ model/register.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose")
 
 const registerSchema = new mongoose.Schema({
     name: String,
-    email: String,
+    email: { type: String, unique:true},
     password: String
 });
 
